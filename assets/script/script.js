45c9ecb97d1fbde8f62c3f0502704e22ba4fdc2d
Hexuan Li <lihexuan1@gmail.com>
2022-04-06 21:42:10 -0700

    add local storage

--- assets/script/script.js
+++ assets/script/script.js
@@ -7,7 +7,7 @@
 var timeHeading = moment().format("LLLL");
 var timeBlock =$(".time-block");
 var hourNow =moment().minutes(0).seconds(0).milliseconds(0);
-
+var saveBtn = $(".saveBtn");
 
 $("#currentDay").text(timeHeading);
 
@@ -19,18 +19,17 @@ $("#currentDay").text(timeHeading);
 
 // WHEN I view the timeblocks for that day
 // THEN each timeblock is color coded to indicate whether it is in the past, present, or future
-    //create variables for each (past, present ,future)
-
 
 var hour = $(".hour");
-var hourStart = moment().hour(16).minutes(0).seconds(0).milliseconds(0);
+//make the first timeblock row shows 9:00am, so always start from the hour(8) of the current day.
+var hourStart = moment().hour(8).minutes(0).seconds(0).milliseconds(0);
 
-
+ //write a function using a "if..else if ...else" to determine the time is past, present or future
 for (var i = 0; i < hour.length; i++ ) {
     var hourBlock = hourStart.add(1,"h")
- console.log(hourBlock);
- console.log(hour.length);
+    //using .isBefore .isSame and .isAfter to compare the timeblock row to current time.
     if (hourBlock.isBefore(hourNow)) {
+        //add css class to the timeblock row respectively
             $(timeBlock.children().eq(i)).addClass("past")
     } 
         else if (hourBlock.isSame(hourNow)) {
@@ -41,20 +40,60 @@ for (var i = 0; i < hour.length; i++ ) {
     }
 }
 
-
-
-    //write a function using a "if..else if ...else" to determine the time is past, present or future
-    //add css class to the determined variables respectively
-
 // WHEN I click into a timeblock
 // THEN I can enter an event
     //a variable that collect user input
-var event = $(".textarea")
+var userMessage = $(".textarea")
+console.log(userMessage)
 // WHEN I click the save button for that timeblock
 // THEN the text for that event is saved in local storage
+
+// get user message from local storage
+function getInfo (n) {
+    var userInput =localStorage.getItem("Todo-" + n);
+    
+    if (userInput !== null ){
+        todoList = JSON.parse(userInput);
+        return todoList;
+    } else {
+        todoList = [];
+    }
+    return todoList;
+};
+
+// add user message input to the local storage
+
+
+function addInfo (n,m) {
+    var addedList = [];
+    addedList.push(m);
+    localStorage.setItem("Todo-" + n, JSON.stringify(addedList));
+};
+
+function saveInfo (n) {
+    var message =$(userMessage.parent().eq(n).children().eq(1)).val();
+    console.log(userMessage)
+    addInfo(n,message);
+}
     //user input can be saved in local storage
     //add event listerner to the save btn
 
 // WHEN I refresh the page
 // THEN the saved events persist
     //render the local storage data when page loads
+
+function renderInfo(n) {
+    $(userMessage.parent().eq(n).children().eq(1)).text(getInfo);
+    
+}
+renderInfo();
+
+
+// $(saveBtn.parent().eq(n).children().eq(2)).on("click",saveInfo);
+
+
+for (var i = 0; i< hour.length; i++){
+    renderInfo(i);
+    saveInfo(i);
+    $(saveBtn.parent().eq(i).children().eq(2)).on("click",saveInfo(i));
+}
