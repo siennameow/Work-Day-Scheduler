abf1c4ecdc0c227781a476b011df1d60af050b8a
Hexuan Li <lihexuan1@gmail.com>
2022-04-06 19:25:08 -0700

    create function to show color to past, oresent, and future

--- assets/script/script.js
+++ assets/script/script.js
@@ -4,6 +4,14 @@
 // WHEN I open the planner
 // THEN the current day is displayed at the top of the calendar
     //set timmer using moment.js
+var timeHeading = moment().format("LLLL");
+var timeBlock =$(".time-block");
+var hourNow =moment().minutes(0).seconds(0).milliseconds(0);
+
+
+$("#currentDay").text(timeHeading);
+
+
 
 // WHEN I scroll down
 // THEN I am presented with timeblocks for standard business hours
@@ -12,13 +20,36 @@
 // WHEN I view the timeblocks for that day
 // THEN each timeblock is color coded to indicate whether it is in the past, present, or future
     //create variables for each (past, present ,future)
+
+
+var hour = $(".hour");
+var hourStart = moment().hour(16).minutes(0).seconds(0).milliseconds(0);
+
+
+for (var i = 0; i < hour.length; i++ ) {
+    var hourBlock = hourStart.add(1,"h")
+ console.log(hourBlock);
+ console.log(hour.length);
+    if (hourBlock.isBefore(hourNow)) {
+            $(timeBlock.children().eq(i)).addClass("past")
+    } 
+        else if (hourBlock.isSame(hourNow)) {
+            $(timeBlock.children().eq(i)).addClass("present")
+    }      
+        else { 
+            $(timeBlock.children().eq(i)).addClass("future")
+    }
+}
+
+
+
     //write a function using a "if..else if ...else" to determine the time is past, present or future
     //add css class to the determined variables respectively
 
 // WHEN I click into a timeblock
 // THEN I can enter an event
     //a variable that collect user input
-
+var event = $(".textarea")
 // WHEN I click the save button for that timeblock
 // THEN the text for that event is saved in local storage
     //user input can be saved in local storage
